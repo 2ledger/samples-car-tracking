abf379057cb4f3632449defe1b5603bb2b4fe336
Marcio Alexandre Macedo Rodrigues <marcioamr@MacBook-Pro-de-Marcio.local>
2018-05-20 21:57:07 -0300

    Path fix

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -1,11 +1,11 @@
 module.exports = function(app) {
-	var carTracking = app.controllers.carTrackingController;
+	var tracking = app.controllers.trackingController;
 
-	app.get('/carTracking/getToken', carTracking.getToken);
-	app.get('/carTracking/getAllParts', carTracking.getAllParts);
-	app.get('/carTracking/verifyCar/:siglaCar', carTracking.verifyCar);	
-	app.post('/carTracking/savePart' , carTracking.savePart);
-	app.post('/carTracking/moveCar' , carTracking.moveCar);
-	app.post('/carTracking/saveCar' , carTracking.saveCar);
-	app.delete('/carTracking/deletePart', carTracking.deletePart);
+	app.get('/tracking/getToken', tracking.getToken);
+	app.get('/tracking/getAllParts', tracking.getAllParts);
+	app.get('/tracking/verifyCar/:siglaCar', tracking.verifyCar);	
+	app.post('/tracking/savePart' , tracking.savePart);
+	app.post('/tracking/moveCar' , tracking.moveCar);
+	app.post('/tracking/saveCar' , tracking.saveCar);
+	app.delete('/tracking/deletePart', tracking.deletePart);
 };
