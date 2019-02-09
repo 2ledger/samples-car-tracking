405f61b6c4d96e6639b1f5103d06dbc45fcbffba
Emmanuel Kiametis <kiametis91@gmail.com>
2019-02-09 00:36:19 -0200

    Corrigindo variaveis

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ var development = {
 	env : global.process.env.NODE_ENV || 'development',
 	port : global.process.env.PORT || 8080,
 	host : global.process.env.HOST || 'localhost',
-	API_2LEDGER : global.process.env.API_2LEDGER || '2ledger-api-dev.azurewebsites.net',
-	API_2LEDGER_SAMPLE_ID_NETWORK : '5ae262c2db26860036e5bd96',
+	API_2LEDGER : global.process.env.API_2LEDGER || '2ledger-api-dev-teste.azurewebsites.net',
+	API_2LEDGER_SAMPLE_ID_NETWORK : '5c5e31d01f76dd0095141274',
 	API_2LEDGER_TOKEN : global.process.env.API_2LEDGER_TOKEN || '.',
 	API_2LEDGER_BASIC_LOGIN : global.process.env.API_BASIC_LOGIN_2LEDGER || 'YWRtX1VzZXJNYW5hZ2VyQGdtYWlsLmNvbToxMjM0NTY=',
-	API_2LEDGER_SAMPLE_CLIENT_ENTITY_ID : global.process.env.API_2LEDGER_SAMPLE_CLIENT_ENTITY_ID || '5b0f1f1029b63b00396c6456'
+	API_2LEDGER_SAMPLE_CLIENT_ENTITY_ID : global.process.env.API_2LEDGER_SAMPLE_CLIENT_ENTITY_ID || '5c5e3c7d1f76dd0095141294'
 };
 
 var production = {
@@ -14,7 +14,7 @@ var production = {
 	port : 8080,
 	host : global.process.env.HOST || 'localhost',
 	API_2LEDGER : global.process.env.API_2LEDGER,
-	API_2LEDGER_SAMPLE_ID_NETWORK : '5ae262c2db26860036e5bd96',
+	API_2LEDGER_SAMPLE_ID_NETWORK : '5c5e31d01f76dd0095141274',
 	API_2LEDGER_TOKEN : global.process.env.API_2LEDGER_TOKEN,
 	API_2LEDGER_BASIC_LOGIN : global.process.env.API_BASIC_LOGIN_2LEDGER || 'YWRtX1VzZXJNYW5hZ2VyQGdtYWlsLmNvbToxMjM0NTY=',
 	API_2LEDGER_SAMPLE_CLIENT_ENTITY_ID : global.process.env.API_2LEDGER_SAMPLE_CLIENT_ENTITY_ID
